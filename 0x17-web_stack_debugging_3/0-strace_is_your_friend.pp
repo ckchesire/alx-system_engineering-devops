9e605c461fdf9b05504f7c5dbddfe857282ae768
Christian Chesire <christianck.chesire@gmail.com>
2025-02-26 21:22:36 +0000

    Updated#2 puppet file that solvers server error.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Solves issue with extension, renaming "phpp" to "php" in "wp-settings.php"
+# Solves wordpress config issue, changing "phpp" to "php" in "wp-settings.php"
 exec {'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
