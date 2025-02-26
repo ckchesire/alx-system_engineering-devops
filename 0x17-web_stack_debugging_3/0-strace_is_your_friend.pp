00308a80e08346091c50a7446255ae5b4e5e4b7e
Christian Chesire <christianck.chesire@gmail.com>
2025-02-26 21:13:31 +0000

    Updated puppet file that solvers server error.

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Solves issue with typo in extension, renaming "phpp" to "php" in "wp-settings.php"
 
-exec {'fix-typo':
+exec {'fix-wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
