1f9c909d52735a1d6590fcd4c533d3f9255e55e6
christian chesire <christianck.chesire@gmail.com>
2025-03-06 03:31:12 +0000

    Updated#1 puppet file to be in line with puppet-lint.

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,17 @@
 # Puppet file to increase the amout of traffic nginx server can handle
 
 # Run executable to increase the ULIMIT of the default file
-exec { 'update-for-nginx':
+exec { 'fix-for-nginx':
   # Update the ULIMIT value
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   # Specify the particular path for the sed command
-  path    => '/usr/local/bin/:/bin/',
+  path    => '/usr/local/bin/:/bin/'
 } ->
 
 # Restart the Nginx server
-exec { 'nginx-server-restart':
+exec { 'nginx-restart':
   # Restart the Nginx service
-  command => '	nginx restart',
+  command => 'nginx restart',
   # Specify the absolute path for the init.d script
-  path    => '/etc/init.d/',
+  path    => '/etc/init.d/'
 }
